1eb26b89d5cc2f4dbca25e05f2efe7f18a0e47b1
kagomax <maxime@group.octan>
2021-04-21 14:19:41 +0200

    designed the home page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { React } from 'react';
 import Header from './components/Header.js'
+import Home from './components/Home.js';
 
 const App = () => {
   return (
     <>
     <Header /> 
+    <Home />
     </>
   )
 }
