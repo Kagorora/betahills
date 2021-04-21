9224697560c51d9041669d17d620996d1d07baea
kagomax <maxime@group.octan>
2021-04-21 16:10:40 +0200

    designed the home page && information page

---
+++ src/components/Seamless.js
@@ -0,0 +1,32 @@
+import { React } from 'react';
+
+const Seamlessly = () => {
+    return (
+        <div className="w-full h-screen bg-green-300 pl-20 pr-20">
+            <div className="pt-40 text-blue-900 text-6xl mb-3.5 pl-20 pr-20">
+                <p>
+                    Seamless intergation.<br></br>
+                  Thorough testing.<br></br> Better results.
+                </p>
+            </div>
+
+            <div className="float-right pl-20 pr-20 text-xs text-blue-900 mr-60 leading-relaxed	tracking-widest">
+                <p>
+                    Our software testers are led by project managers and <br></br>
+                    lead developers from the US, Canada and Europe with <br></br>
+                    more than 10 years of experience in QA and software <br></br>
+                    testing. This guarantees clear communication and a <br></br>
+                    team with an in-depth understanding of your business <br></br>
+                    and testing needs. After an initial assessment, we <br></br>
+                    tailor our workflow to seamlessly integrate into your <br></br>
+                    current processes. We can use your preferred tools and <br></br>
+                    adapt our services based on your communication <br></br>
+                    models, timeline, and more.<br></br>
+                </p>
+            </div>
+
+        </div>
+    )
+}
+
+export default Seamlessly;
