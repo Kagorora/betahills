ba75d22cacf4ab4da6f3bef6c6a02b2959424c49
kagomax <maxime@group.octan>
2021-04-22 12:47:22 +0200

    fixed the all the pages

--- src/components/Information.js
+++ src/components/Information.js
@@ -2,26 +2,26 @@ import React from 'react';
 
 const Information = () => {
     return (
-        <div className="bg-white h-screen w-full">
-            <div className="pt-40 text-blue-700 text-6xl mb-3.5 pl-20 pr-20">
-            <p>
-                  Our expert testers<br></br>
-                  handle quality assurance<br></br> so you can focus on<br></br> buidling your ideas.</p>
+        <div className="Information bg-white h-screen w-full">
+            <div className="header-list pt-40 text-6xl mb-9 w-3/5">
+                <p>
+                    Our expert testers
+                  handle quality assurance so you can focus on buidling your ideas.</p>
             </div>
 
-            <div className="float-right pl-20 pr-20 text-xs text-blue-700 mr-60 tracking-widest leading-loose">
+            <div className="information-text w-1/2 ml-auto">
                 <p>
-                Betahills was founded by Lena Simon in Berlin in 2019.<br></br>
-                We connect European businesses with a team of female <br></br>
-                software testers in Rwanda. Our team seamlessly<br></br> 
-                integrates into your existing processes at no additional<br></br> 
-                cost and makes sure your solution is flawless and<br></br> 
-                ready for market. Our certified testers are part of <br></br>
-                Rwanda’s first and only QA and software testing team<br></br>
-                and work together with experienced technical leads and <br></br>
-                project managers from the US, Canada and Europe. <br></br>
-                Betahills connects these female tech professionals in <br></br>
-                Rwanda with an international client base.
+                    Betahills was founded by Lena Simon in Berlin in 2019.
+                    We connect European businesses with a team of female
+                    software testers in Rwanda. Our team seamlessly
+                    integrates into your existing processes at no additional
+                    cost and makes sure your solution is flawless and
+                    ready for market. Our certified testers are part of
+                    Rwanda’s first and only QA and software testing team
+                    and work together with experienced technical leads and
+                    project managers from the US, Canada and Europe.
+                    Betahills connects these female tech professionals in
+                    Rwanda with an international client base.
                 </p>
             </div>
         </div>
--- src/components/Testing.js
+++ src/components/Testing.js
@@ -2,23 +2,23 @@ import React from 'react';
 
 const Testing = () => {
     return (
-        <div className="w-full bg-white pl-20 pr-20 pt-40 pb-10">
+        <div className="Testing w-full bg-white pt-40 pb-10">
 
-            <div className="w-full flex flex-row pl-20 mb-28">
-                <div className="text-blue-700 text-6xl  bg-red-900 ">
+            <div className="w-full mb-28">
+                <div className="header-list text-6xl  bg-red-900 w-1/2">
                     <p className="bg-white h-full w-full">Manual testing</p>
                 </div>
 
-                <div className="pl-24 pr-20 text-xs text-blue-700 leading-relaxed	tracking-widest mt-24">
-                    <p className="mb-4">We take the role of the user and test your products into<br></br>
+                <div className="w-1/2 ml-auto information-text">
+                    <p className="mb-4">We take the role of the user and test your products into
                     terms of functionality, design, and usability.
                 </p>
 
-                    <p className="mb-5">We offer the following manual testing services for web,<br></br>
+                    <p className="mb-5">We offer the following manual testing services for web,
                     mobile, and desktop apps.
                 </p>
 
-                    <p className="ml-4">
+                    <p className="">
                         <ul className="list-disc">
                             <li>Test plan creation</li>
                             <li>Functional testing</li>
@@ -31,21 +31,21 @@ const Testing = () => {
             </div>
 
 
-            <div className="w-full mb-16 flex flex-row pl-20 mb-28">
-                <div className="text-blue-700 text-6xl  bg-red-900">
+            <div className="w-full mb-28">
+            <div className="header-list text-6xl  bg-red-900 w-1/2">
                     <p className="bg-white h-full w-full">Automated Testing</p>
                 </div>
 
-                <div className="pr-28 text-xs text-blue-700 leading-relaxed	tracking-widest mt-24">
-                    <p className="mb-4">Save time and increase your testing coverage with<br></br>
+                <div className="w-1/2 ml-auto information-text">
+                    <p className="mb-4">Save time and increase your testing coverage with
                     automated testing.
                 </p>
 
-                    <p className="mb-5">We offer the following the automated services for<br></br>
+                    <p className="mb-5">We offer the following the automated services for
                     web, mobile, and desktop apps.
                 </p>
 
-                    <p className="ml-4">
+                    <p className="">
                         <ul className="list-disc">
                             <li>Api testing</li>
                             <li>Regression testing</li>
@@ -57,14 +57,14 @@ const Testing = () => {
                 </div>
             </div>
 
-            <div className="w-full mb-16 flex flex-row pl-20 mb-28">
-                <div className="text-blue-700 text-6xl  bg-red-900">
+            <div className="w-full">
+            <div className="header-list text-6xl  bg-red-900 w-1/2">
                     <p className="bg-white h-full w-full">Perfomance Testing</p>
                 </div>
 
-                <div className="pr-32 text-xs text-blue-700 leading-relaxed	tracking-widest mt-24">
+                <div className="w-1/2 ml-auto information-text">
                     <p className="mb-4">
-                    Ensure customer satisfaction by guaranteeing optimal<br></br> performance during standard and peak workloads. We<br></br> identify key performance criteria, provide you with <br></br>information about speed, stability and scalability, and <br></br> determine which areas need improvement.<br></br>
+                    Ensure customer satisfaction by guaranteeing optimal performance during standard and peak workloads. We identify key performance criteria, provide you with information about speed, stability and scalability, and  determine which areas need improvement.
                     automated testing.
                 </p>
                 </div>
